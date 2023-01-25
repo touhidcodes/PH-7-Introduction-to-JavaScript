15a932ceb55d9792c42c7934a73d9d3720a190be
Touhidur Zaman <touhidcodes@gmail.com>
2023-01-25 22:40:04 +0600

    essential-math-list

---
+++ math.js
@@ -0,0 +1,18 @@
+/*
+Essential Maths:
+1. Number: 2, 5, odd, even
+2. Integer: 5, 7
+3. Float: 4.5, 1/2
+4. Operation: Addition, Subtraction, Multiplication, Division, Modulus/Reminder
+5. Percent
+6. Average 
+7. Comparison: >, <,  ==, !=, >=, <=
+8. Unitary
+9. Min, Max, Sum, Avg
+10. Series
+
+:::Extra:::
+1. Prime
+2. Permutation
+3. Area
+*/
