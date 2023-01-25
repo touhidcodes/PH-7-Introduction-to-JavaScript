889ec85460518139595e888ba00cbc08d0a33f6a
Touhidur Zaman <touhidcodes@gmail.com>
2023-01-25 23:20:44 +0600

    variable-data-types

--- variable.js
+++ variable.js
@@ -1,5 +1,5 @@
 /*
-% Rules od Variable:
+% Rules of Variable:
 1. Var: The Var Statement Declares a Variable. 
 2. Variable Name: Variables Must be Identified with Unique Names.
 3. Assignment Operator: Use an "=" Operator to Assign a Value.
@@ -7,8 +7,18 @@
 5. Semicolon: Use an ";" as a Delimiter between Key/Value of Data Type and Specifies end of the Var Statement. 
 */
 
+// Variable Data Types:
+// Numeric
 var age = 23;
 var temperature = 38;
 var money = 500;
 var weight = 65;
 var notes = "done";
+
+// String
+var name = "touhid";
+var person = "good";
+
+// Boolean
+var isHappy = true;
+var passed = false;
