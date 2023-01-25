9e78f869ca1e630b33e5c077457105c8a69d1f95
Touhidur Zaman <touhidcodes@gmail.com>
2023-01-25 23:05:05 +0600

    variable

---
+++ variable.js
@@ -0,0 +1,14 @@
+/*
+% Rules od Variable:
+1. Var: The Var Statement Declares a Variable. 
+2. Variable Name: Variables Must be Identified with Unique Names.
+3. Assignment Operator: Use an "=" Operator to Assign a Value.
+4. Value: Specifies the Value to Assign to the Variable.
+5. Semicolon: Use an ";" as a Delimiter between Key/Value of Data Type and Specifies end of the Var Statement. 
+*/
+
+var age = 23;
+var temperature = 38;
+var money = 500;
+var weight = 65;
+var notes = "done";
