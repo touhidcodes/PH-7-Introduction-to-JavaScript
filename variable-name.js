9cb3bd37519229c22c9fde0622385111a565143d
Touhidur Zaman <touhidcodes@gmail.com>
2023-01-25 23:48:10 +0600

    variable-name-rules

---
+++ variable-name.js
@@ -0,0 +1,16 @@
+/*
+Variable Name Rules:
+1. Variable Name can not be any Keyword.
+2. Variable Name has to be a Single Word.
+3. Variable Name can not have Quotation.
+4. Variable Name can nit Starts with Number, But Ends with Number.
+5. Variable Name cannot Use Dash, but Use Underscores.
+6. Use Some Cases: 
+ a. var user_home_address  // Snake Case
+ b. var UserHome-Address  //Pascal Case
+ c. var userHomeAddress  // Camel Case ***
+*/
+
+var homeAddress = "jashore";
+var HomeAdress = "jashore";
+var home_address = "jashore";
